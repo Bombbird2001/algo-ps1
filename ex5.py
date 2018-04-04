2708aa92d001a7b09ba917a0dbec3de22ddf79e0
Bombbird2001 <kushangyi2001@gmail.com>
2018-04-04 16:18:52 +0800

    Submit ps1
    Wiggly potats

--- ex5.py
+++ ex5.py
@@ -1,1 +1,11 @@
 # Type your code for insertion sort (Exercise 5) here
+numbers = list(map(int, input("Enter a list of numbers:").split(" ")))
+for i in range(1, len(numbers)):
+	key = numbers[i]
+	k = i - 1
+	while k >= 0 and key < numbers[k]:
+		(numbers[i], numbers[k]) = (numbers[k], numbers[i])
+		i -= 1;
+		k -= 1;
+
+print(numbers)
--- ex6.py
+++ ex6.py
@@ -1,1 +1,23 @@
 # Type your code for binary search (Exercise 6) here
+from random import randint
+magicNo = randint(1, 99)
+low = 0
+high = 100
+guesses = 0
+while True:
+	guess = (low + high) // 2
+	guesses += 1
+	if guesses > 1000:
+		#Just in case something goes wrong
+		print("Something went wrong!")
+		break
+	print("Guess " + str(guesses) + ":", guess)
+	if guess < magicNo:
+		low = guess
+		print("Too low!")
+	elif guess > magicNo:
+		high = guess
+		print("Too high!")
+	else:
+		print("magicNo is", magicNo)
+		break
