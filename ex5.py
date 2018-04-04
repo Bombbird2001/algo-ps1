a84857c7b7253c9a5cbb0602dd46e723130ccb75
Bombbird2001 <kushangyi2001@gmail.com>
2018-04-04 16:22:42 +0800

    Some stuff
    Prevent user from inputting screwed up formats

--- ex5.py
+++ ex5.py
@@ -1,5 +1,11 @@
 # Type your code for insertion sort (Exercise 5) here
-numbers = list(map(int, input("Enter a list of numbers:").split(" ")))
+while True:
+	try:
+		numbers = list(map(int, input("Enter a list of numbers:").split(" ")))
+		break
+	except:
+		print("Please enter a valid list of space-separated integers!")
+
 for i in range(1, len(numbers)):
 	key = numbers[i]
 	k = i - 1
