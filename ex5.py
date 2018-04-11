89c160ccbc9885cc13b88d1f4a5ef6445c0beadb
Bombbird2001 <kushangyi2001@gmail.com>
2018-04-11 19:28:42 +0800

    Minor changes

--- ex5.py
+++ ex5.py
@@ -1,17 +1,22 @@
 # Type your code for insertion sort (Exercise 5) here
+def insertion_sort(noList):
+	for i in range(1, len(numbers)):
+		key = numbers[i]
+		k = i - 1
+		while k >= 0 and key < numbers[k]:
+			(numbers[i], numbers[k]) = (numbers[k], numbers[i])
+			i -= 1;
+			k -= 1;
+
+	print(numbers)
+
 while True:
-	try:
-		numbers = list(map(int, input("Enter a list of numbers:").split(" ")))
-		break
-	except:
-		print("Please enter a valid list of space-separated integers!")
-
-for i in range(1, len(numbers)):
-	key = numbers[i]
-	k = i - 1
-	while k >= 0 and key < numbers[k]:
-		(numbers[i], numbers[k]) = (numbers[k], numbers[i])
-		i -= 1;
-		k -= 1;
-
-print(numbers)
+		try:
+			rawInput = input("Enter a list of numbers, or X to exit:")
+			if rawInput == "X":
+				break
+			else:
+				numbers = list(map(int, rawInput.split(" ")))
+				insertion_sort(numbers)
+		except:
+			print("Please enter a valid list of space-separated integers!")
